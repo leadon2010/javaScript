ab94d87a399dee6eab9843689a0d97565dd71e01
spring <spring@DESKTOP-D0U3BLT>
2018-08-26 21:51:36 +0900

    11

--- javaScript/src/common/BoardFileUpload.java
+++ javaScript/src/common/BoardFileUpload.java
@@ -16,43 +16,44 @@ import javax.servlet.http.Part;
  * Servlet implementation class BoardFileUpload 상품 이미지 서블릿
  */
 @WebServlet("/common/BoardFileUpload")
-@MultipartConfig(location = "c:/tmp", 
-				 maxFileSize = 1024000L, 
-				 maxRequestSize = -1, 
-				 fileSizeThreshold = 1024)
+@MultipartConfig(location = "c:/tmp", maxFileSize = 1024000L, maxRequestSize = -1, fileSizeThreshold = 1024)
 public class BoardFileUpload extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BoardFileUpload() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public BoardFileUpload() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Collection<Part> parts=request.getParts();
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Collection<Part> parts = request.getParts();
 		String filename = "";
-		String filepath = request.getServletContext().getRealPath("/upload"); //경로
+		String filepath = request.getServletContext().getRealPath("/upload"); // 경로
 		System.out.println(filepath);
-		for(Part part : parts) {
+		for (Part part : parts) {
 			filename = part.getSubmittedFileName();
 			System.out.println("filename: " + part.getSubmittedFileName());
 			System.out.println("filesize: " + part.getSize());
 			System.out.println("parameter name: " + part.getName());
-			part.write(filepath+ "/" + part.getSubmittedFileName()); //경로/파일명
+			part.write(filepath + "/" + part.getSubmittedFileName()); // 경로/파일명
 		}
 		PrintWriter out = response.getWriter();
 		out.println("<script>");
