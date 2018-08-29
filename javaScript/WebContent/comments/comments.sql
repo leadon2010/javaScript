b58e1ef544b5d5b40156e9b96d2db8ac194d424d
spring <spring@DESKTOP-D0U3BLT>
2018-08-29 09:21:47 +0900

    66

--- javaScript/WebContent/comments/comments.sql
+++ javaScript/WebContent/comments/comments.sql
@@ -16,3 +16,21 @@ tablespace USERS
     minextents 1
     maxextents unlimited
   );
+
+  -- Create table
+create table ID_REPOSITORY
+(
+  value NUMBER,
+  name  VARCHAR2(30)
+)
+tablespace USERS
+  pctfree 10
+  initrans 1
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    next 1M
+    minextents 1
+    maxextents unlimited
+  );
