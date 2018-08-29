2b2cf2d682069fc28ad295dee3a0cbafa5792600
spring <spring@DESKTOP-D0U3BLT>
2018-08-29 09:17:06 +0900

    11

---
+++ javaScript/WebContent/comments/comments.sql
@@ -0,0 +1,18 @@
+-- Create table
+create table COMMENTS
+(
+  id      VARCHAR2(10),
+  name    VARCHAR2(100),
+  content VARCHAR2(300)
+)
+tablespace USERS
+  pctfree 10
+  initrans 1
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    next 1M
+    minextents 1
+    maxextents unlimited
+  );
