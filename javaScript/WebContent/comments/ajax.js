f44eb2a3063e6309347f100e3fb969ff067a025a
winny <winny@DESKTOP-D49K3C9>
2020-03-28 18:37:20 +0900

    jkl

--- javaScript/WebContent/comments/ajax.js
+++ javaScript/WebContent/comments/ajax.js
@@ -1,54 +1,52 @@
 var ajax = {};
 ajax.xhr = {};
 
-ajax.xhr.Request = function(url, params, callback, method, applyObj) {
+ajax.xhr.Request = function (url, params, callback, method, applyObj) {
 	this.url = url;
 	this.params = params;
 	this.callback = callback;
 	this.method = method;
 	this.applyObj = (applyObj == null) ? null : applyObj;
-	
+
 	this.send();
 }
 ajax.xhr.Request.prototype = {
-	getXMLHttpRequest: function() {
+	getXMLHttpRequest: function () {
 		if (window.ActiveXObject) {
 			try {
 				return new ActiveXObject("Msxml2.XMLHTTP");
-			} catch(e) {
+			} catch (e) {
 				try {
 					return new ActiveXObject("Microsoft.XMLHTTP");
-				} catch(e1) { return null; }
+				} catch (e1) { return null; }
 			}
 		} else if (window.XMLHttpRequest) {
 			return new XMLHttpRequest();
 		} else {
 			return null;
-		}		
+		}
 	},
-	send: function() {
+	send: function () {
 		this.req = this.getXMLHttpRequest();
-		
+
 		var httpMethod = this.method ? this.method : 'GET';
 		if (httpMethod != 'GET' && httpMethod != 'POST') {
 			httpMethod = 'GET';
 		}
-		var httpParams = (this.params == null || this.params == '') ? 
-		                 null : this.params;
+		var httpParams = (this.params == null || this.params == '') ? null : this.params;
 		var httpUrl = this.url;
 		if (httpMethod == 'GET' && httpParams != null) {
 			httpUrl = httpUrl + "?" + httpParams;
 		}
 		this.req.open(httpMethod, httpUrl, true);
-		this.req.setRequestHeader(
-			'Content-Type', 'application/x-www-form-urlencoded');
+		this.req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 		var request = this;
-		this.req.onreadystatechange = function() {
+		this.req.onreadystatechange = function () {
 			request.onStateChange.call(request);
 		}
 		this.req.send(httpMethod == 'POST' ? httpParams : null);
 	},
-	onStateChange: function() {
+	onStateChange: function () {
 		if (this.applyObj) {
 			this.callback.call(this.applyObj, this.req);
 		} else {
@@ -58,8 +56,8 @@ ajax.xhr.Request.prototype = {
 }
 
 ajax.Event = {};
-ajax.Event.addListener = function(element, name, observer, useCapture) {
-    useCapture = useCapture || false;
+ajax.Event.addListener = function (element, name, observer, useCapture) {
+	useCapture = useCapture || false;
 
 	if (element.addEventListener) {
 		element.addEventListener(name, observer, useCapture);
@@ -67,25 +65,25 @@ ajax.Event.addListener = function(element, name, observer, useCapture) {
 		element.attachEvent('on' + name, observer);
 	}
 }
-ajax.Event.removeListener = function(element, name, observer, useCapture) {
+ajax.Event.removeListener = function (element, name, observer, useCapture) {
 	useCapture = useCapture || false;
-	
+
 	if (element.removeEventListener) {
 		element.removeEventListener(name, observer, useCapture);
 	} else if (element.detachEvent) {
 		element.detachEvent('on' + name, observer);
 	}
 }
-ajax.Event.getTarget = function(event) {
+ajax.Event.getTarget = function (event) {
 	if (event == null) return null;
 	if (event.target) return event.target;
 	else if (event.srcElement) return event.srcElement;
 	return null;
 }
-ajax.Event.getMouseXY = function(event) {
+ajax.Event.getMouseXY = function (event) {
 	var mouseX = event.clientX;
 	var mouseY = event.clientY;
-	
+
 	var dd = document.documentElement;
 	var db = document.body;
 	if (dd) {
@@ -95,68 +93,67 @@ ajax.Event.getMouseXY = function(event) {
 		mouseX += db.scrollLeft;
 		mouseY += db.scrollTop;
 	}
-	return {x: mouseX, y: mouseY};
+	return { x: mouseX, y: mouseY };
 }
-ajax.Event.isLeftButton= function(event) {
-	return (event.which) ? 
-	       event.which == 1 && event.button == 0 :
-	       (event.type == 'click') ? event.button == 0 : event.button == 1;
+ajax.Event.isLeftButton = function (event) {
+	return (event.which) ?
+		event.which == 1 && event.button == 0 :
+		(event.type == 'click') ? event.button == 0 : event.button == 1;
 }
-ajax.Event.isRightButton = function(event) {
+ajax.Event.isRightButton = function (event) {
 	return event.button == 2;
 }
-ajax.Event.stopPropagation = function(event) {
+ajax.Event.stopPropagation = function (event) {
 	if (event.stopPropagation) {
-	    event.stopPropagation();
+		event.stopPropagation();
 	} else {
-	    event.cancelBubble = true;
+		event.cancelBubble = true;
 	}
 }
-ajax.Event.preventDefault = function(event) {
+ajax.Event.preventDefault = function (event) {
 	if (event.preventDefault) {
-	    event.preventDefault();
+		event.preventDefault();
 	} else {
-	    event.returnValue = false;
+		event.returnValue = false;
 	}
 }
-ajax.Event.stopEvent = function(event) {
+ajax.Event.stopEvent = function (event) {
 	ajax.Event.stopPropagation(event);
 	ajax.Event.preventDefault(event);
 }
-ajax.Event.bindAsListener = function(func, obj) {
-	return function() {
+ajax.Event.bindAsListener = function (func, obj) {
+	return function () {
 		return func.apply(obj, arguments);
 	}
 }
 
 ajax.GUI = {};
-ajax.GUI.setOpacity = function(el, opacity) {
+ajax.GUI.setOpacity = function (el, opacity) {
 	if (el.filters) {
 		el.style.filter = 'alpha(opacity=' + opacity * 100 + ')';
 	} else {
 		el.style.opacity = opacity;
 	}
 }
-ajax.GUI.getStyle = function(el, property) {
+ajax.GUI.getStyle = function (el, property) {
 	var value = null;
 	var dv = document.defaultView;
-	
+
 	if (property == 'opacity' && el.filters) {// IE opacity
 		value = 1;
 		try {
 			value = el.filters.item('alpha').opacity / 100;
-		} catch(e) {}
+		} catch (e) { }
 	} else if (el.style[property]) {
 		value = el.style[property];
 	} else if (el.currentStyle && el.currentStyle[property]) {
 		value = el.currentStyle[property];
-	} else if ( dv && dv.getComputedStyle ) {
-		// ´ë¹®ÀÚ¸¦ ¼Ò¹®ÀÚ·Î º¯È¯ÇÏ°í ±× ¾Õ¿¡ '-'¸¦ ºÙÀÎ´Ù.
+	} else if (dv && dv.getComputedStyle) {
+		// ï¿½ë¹®ï¿½Ú¸ï¿½ ï¿½Ò¹ï¿½ï¿½Ú·ï¿½ ï¿½ï¿½È¯ï¿½Ï°ï¿½ ï¿½ï¿½ ï¿½Õ¿ï¿½ '-'ï¿½ï¿½ ï¿½ï¿½ï¿½Î´ï¿½.
 		var converted = '';
-		for(i = 0, len = property.length;i < len; ++i) {
+		for (i = 0, len = property.length; i < len; ++i) {
 			if (property.charAt(i) == property.charAt(i).toUpperCase()) {
-				converted = converted + '-' + 
-				            property.charAt(i).toLowerCase();
+				converted = converted + '-' + property.charAt(i).toLowerCase();
 			} else {
 				converted = converted + property.charAt(i);
 			}
@@ -168,20 +165,20 @@ ajax.GUI.getStyle = function(el, property) {
 	return value;
 }
 
-ajax.GUI.getXY = function(el) {
-	// elÀº ¹®¼­¿¡ Æ÷ÇÔµÇ¾î ÀÖ¾î¾ß ÇÏ°í, È­¸é¿¡ º¸¿©¾ß ÇÑ´Ù.
+ajax.GUI.getXY = function (el) {
+	// elï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ÔµÇ¾ï¿½ ï¿½Ö¾ï¿½ï¿½ ï¿½Ï°ï¿½, È­ï¿½é¿¡ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Ñ´ï¿½.
 	if (el.parentNode === null || el.style.display == 'none') {
 		return false;
 	}
-	
+
 	var parent = null;
 	var pos = [];
 	var box;
-	
-	if (document.getBoxObjectFor) { // gecko ¿£Áø ±â¹Ý
+
+	if (document.getBoxObjectFor) { // gecko ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½
 		box = document.getBoxObjectFor(el);
 		pos = [box.x, box.y];
-	} else { // ±âÅ¸ ºê¶ó¿ìÀú
+	} else { // ï¿½ï¿½Å¸ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 		pos = [el.offsetLeft, el.offsetTop];
 		parent = el.offsetParent;
 		if (parent != el) {
@@ -191,38 +188,38 @@ ajax.GUI.getXY = function(el) {
 				parent = parent.offsetParent;
 			}
 		}
-		// ¿ÀÆä¶ó¿Í »çÆÄ¸®ÀÇ 'absolute' postionÀÇ °æ¿ì
-		// bodyÀÇ offsetTopÀ» Àß¸ø °è»êÇÏ¹Ç·Î º¸Á¤ÇØ¾ß ÇÑ´Ù.
+		// ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½Ä¸ï¿½ï¿½ï¿½ 'absolute' postionï¿½ï¿½ ï¿½ï¿½ï¿½
+		// bodyï¿½ï¿½ offsetTopï¿½ï¿½ ï¿½ß¸ï¿½ ï¿½ï¿½ï¿½ï¿½Ï¹Ç·ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½Ø¾ï¿½ ï¿½Ñ´ï¿½.
 		var ua = navigator.userAgent.toLowerCase();
 		if (
 			ua.indexOf('opera') != -1
-			|| ( ua.indexOf('safari') != -1 && this.getStyle(el, 'position') == 'absolute' )
+			|| (ua.indexOf('safari') != -1 && this.getStyle(el, 'position') == 'absolute')
 		) {
 			pos[1] -= document.body.offsetTop;
 		}
 	}
-	
+
 	if (el.parentNode) { parent = el.parentNode; }
 	else { parent = null; }
-	
-	// body ¶Ç´Â html ÀÌ¿ÜÀÇ ºÎ¸ð ³ëµå Áß¿¡ ½ºÅ©·ÑµÇ¾î ÀÖ´Â
-	// ¿µ¿ªÀÌ ÀÖ´Ù¸é ¾Ë¸Â°Ô Ã³¸®ÇÑ´Ù.
+
+	// body ï¿½Ç´ï¿½ html ï¿½Ì¿ï¿½ï¿½ï¿½ ï¿½Î¸ï¿½ ï¿½ï¿½ï¿½ ï¿½ß¿ï¿½ ï¿½ï¿½Å©ï¿½ÑµÇ¾ï¿½ ï¿½Ö´ï¿½
+	// ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Ö´Ù¸ï¿½ ï¿½Ë¸Â°ï¿½ Ã³ï¿½ï¿½ï¿½Ñ´ï¿½.
 	while (parent && parent.tagName != 'BODY' && parent.tagName != 'HTML') {
 		pos[0] -= parent.scrollLeft;
 		pos[1] -= parent.scrollTop;
-		
+
 		if (parent.parentNode) { parent = parent.parentNode; }
 		else { parent = null; }
 	}
-	return {x: pos[0], y: pos[1]};
+	return { x: pos[0], y: pos[1] };
 }
-ajax.GUI.getX = function(el) {
+ajax.GUI.getX = function (el) {
 	return ajax.GUI.getXY(el).x;
 }
-ajax.GUI.getY = function(el) {
+ajax.GUI.getY = function (el) {
 	return ajax.GUI.getXY(el).y;
 }
-ajax.GUI.getBounds = function(el) {
+ajax.GUI.getBounds = function (el) {
 	var xy = ajax.GUI.getXY(el);
 	return {
 		x: xy.x,
@@ -231,7 +228,7 @@ ajax.GUI.getBounds = function(el) {
 		height: el.offsetHeight
 	};
 }
-ajax.GUI.setXY = function(el, x, y) {
+ajax.GUI.setXY = function (el, x, y) {
 	var pageXY = ajax.GUI.getXY(el);
 	if (pageXY === false) { return false; }
 	var position = ajax.GUI.getStyle(el, 'position');
@@ -239,24 +236,24 @@ ajax.GUI.setXY = function(el, x, y) {
 		el.style.position = 'relative';
 	}
 	var delta = {
-		x: parseInt( ajax.GUI.getStyle(el, 'left'), 10 ),
-		y: parseInt( ajax.GUI.getStyle(el, 'top'), 10 )
+		x: parseInt(ajax.GUI.getStyle(el, 'left'), 10),
+		y: parseInt(ajax.GUI.getStyle(el, 'top'), 10)
 	};
-	if ( isNaN(delta.x) ) { delta.x = 0; }
-	if ( isNaN(delta.y) ) { delta.y = 0; }
-	
+	if (isNaN(delta.x)) { delta.x = 0; }
+	if (isNaN(delta.y)) { delta.y = 0; }
+
 	if (x != null) {
 		el.style.left = (x - pageXY.x + delta.x) + 'px';
 	}
 	if (y != null) {
 		el.style.top = (y - pageXY.y + delta.y) + 'px';
 	}
-	
+
 	return true;
 }
 
 ajax.Class = {
-	extend: function(childsProto, parentFunc, addFunc, parentProto) {
+	extend: function (childsProto, parentFunc, addFunc, parentProto) {
 		if (parentProto != null) {
 			childsProto.parent = parentProto;
 		}
