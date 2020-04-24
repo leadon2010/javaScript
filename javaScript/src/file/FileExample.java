003a377d927697286a350d5df326363bf0cd458c
leadon <leadon@localhost>
2020-04-24 17:14:09 +0900

    dfd

--- javaScript/src/file/FileExample.java
+++ javaScript/src/file/FileExample.java
@@ -4,25 +4,33 @@ import java.io.File;
 
 public class FileExample {
 	public static void main(String[] args) {
-		String path = "/home/ubie/Dev/git/javaScript/javaScript/WebContent";
+
+		String path = "/home/leadon/Dev/git/javaScript/javaScript/WebContent";
 		File temp = new File(path);
 		File[] fileList = temp.listFiles();
+
 		if (fileList.length > 0) {
+
 			for (File file : fileList) {
+
 				System.out.println(file.getName());
+
 				if (file.isDirectory()) {
 					String addPath = path + "/" + file.getName();
 					File addTemp = new File(addPath);
 					File[] addList = addTemp.listFiles();
+
 					for (File addFile : addList) {
 						if (addFile.getName().indexOf("html") > 0)
 							System.out.println("  " + addFile.getName());
 						else if (addFile.getName().indexOf("jsp") > 0)
 							System.out.println("  " + addFile.getName());
 					}
+
 				}
 
-			}
+			} // main loop
+
 		} else {
 			System.out.println("no list");
 		}
