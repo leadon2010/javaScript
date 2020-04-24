1ff9cd3f74206ada50789a46049f0beefd7b22b5
leadon <leadon@localhost>
2020-04-24 23:36:57 +0900

    dfsd

--- javaScript/src/file/FileExample.java
+++ javaScript/src/file/FileExample.java
@@ -3,6 +3,26 @@ package file;
 import java.io.File;
 
 public class FileExample {
+
+	public static File[] sortFileArray(File[] fileArray) {
+		File temp;
+		File[] newAry = null;
+		for (int j = 0; j < fileArray.length - 1; j++) {
+			for (int i = 0; i < fileArray.length - 1; i++) {
+				if ((fileArray[i].isDirectory() ? "/" + fileArray[i].getName() : fileArray[i].getName())
+						.compareToIgnoreCase((fileArray[i + 1].isDirectory() ? "/" + fileArray[i + 1].getName()
+								: fileArray[i + 1].getName())) > 0) {
+					temp = fileArray[i];
+					fileArray[i] = fileArray[i + 1];
+					fileArray[i + 1] = temp;
+				}
+			}
+		}
+		newAry = fileArray;
+
+		return newAry;
+	}
+
 	public static void main(String[] args) {
 		String osName = System.getProperty("user.name");
 		System.out.println(osName);
@@ -10,33 +30,44 @@ public class FileExample {
 		String path = "/home/" + osName + "/Dev/git/javaScript/javaScript/WebContent";
 		File temp = new File(path);
 		File[] fileList = temp.listFiles();
-		
 
-		if (fileList.length > 0) {
+		for (File file : sortFileArray(fileList)) {
 
-			for (File file : fileList) {
+			System.out.println("/" + file.getName());
 
-				System.out.println("/" + file.getName());
+			if (file.isDirectory()) {
+				String addPath = path + "/" + file.getName();
+				File addTemp = new File(addPath);
+				File[] addList = addTemp.listFiles();
 
-				if (file.isDirectory()) {
-					String addPath = path + "/" + file.getName();
-					File addTemp = new File(addPath);
-					File[] addList = addTemp.listFiles();
+				for (File addFile : sortFileArray(addList)) {
+					if (addFile.getName().indexOf("html") != -1 || addFile.getName().indexOf("jsp") != -1) {
+						System.out.println("    " + addFile.getName());
 
-					for (File addFile : addList) {
-						if (addFile.getName().indexOf("html") != -1 || addFile.getName().indexOf("jsp") != -1)
-							System.out.println("  " + addFile.getName());
+					} else if (addFile.isDirectory()) {
+						System.out.println("    /" + addFile.getName());
 
-						else if (addFile.isDirectory())
-							System.out.println("  /" + addFile.getName());
+						String dupPath = path + "/" + file.getName() + "/" + addFile.getName();
+						File dupTemp = new File(dupPath);
+						File[] dupList = dupTemp.listFiles();
+
+						for (File dupFile : sortFileArray(dupList)) {
+							if (dupFile.getName().indexOf("html") != -1 || dupFile.getName().indexOf("jsp") != -1) {
+								System.out.println("        " + dupFile.getName());
+							} else if (dupFile.isDirectory()) {
+								System.out.println("        /" + dupFile.getName());
+							}
+
+						} // dup loop
+
 					}
 
-				}
+				} // sup loop
 
-			} // main loop
+			}
 
-		} else {
-			System.out.println("no list");
-		}
-	}
-}
+		} // main loop
+
+	} // end of main
+
+} // end of class
